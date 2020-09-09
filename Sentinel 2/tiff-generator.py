de5545c7df0f720410d01d151475dc00f4911580
harveyla <liuhao17@mails.ucas.edu.cn>
2020-09-09 20:22:58 +0800

    1C and resample

--- Sentinel 2/tiff-generator.py
+++ Sentinel 2/tiff-generator.py
@@ -77,23 +77,37 @@ def generate_all_bands(unprocessedBandPath, granule, outputPathSubdirectory):
 
     #print("\n\t" + inputPath)
 
-    bands = {"band_AOT" :  inputPath + "R10m/" + granuleBandTemplate +  "AOT_10m.jp2",
-    "band_02" :  inputPath + "R10m/" + granuleBandTemplate +  "B02_10m.jp2",
-    "band_03" :  inputPath + "R10m/" + granuleBandTemplate +  "B03_10m.jp2",
-    "band_04" :  inputPath + "R10m/" + granuleBandTemplate +  "B04_10m.jp2",
-    "band_05" :  inputPath + "R20m/" + granuleBandTemplate +  "B05_20m.jp2",
-    "band_06" :  inputPath + "R20m/" + granuleBandTemplate +  "B06_20m.jp2",
-    "band_07" :  inputPath + "R20m/" + granuleBandTemplate +  "B07_20m.jp2",
-    "band_08" :  inputPath + "R10m/" + granuleBandTemplate +  "B08_10m.jp2",
-    "band_8A" :  inputPath + "R20m/" + granuleBandTemplate +  "B8A_20m.jp2",
-    "band_09" :  inputPath + "R60m/" + granuleBandTemplate +  "B09_60m.jp2",
-    "band_WVP" :  inputPath + "R10m/" + granuleBandTemplate +  "WVP_10m.jp2",
-    "band_11" :  inputPath + "R20m/" + granuleBandTemplate +  "B11_20m.jp2",
-    "band_12" :  inputPath + "R20m/" + granuleBandTemplate +  "B12_20m.jp2"}
+    # bands = {"band_AOT" :  inputPath + "R10m/" + granuleBandTemplate +  "AOT_10m.jp2",
+    # "band_02" :  inputPath + "R10m/" + granuleBandTemplate +  "B02_10m.jp2",
+    # "band_03" :  inputPath + "R10m/" + granuleBandTemplate +  "B03_10m.jp2",
+    # "band_04" :  inputPath + "R10m/" + granuleBandTemplate +  "B04_10m.jp2",
+    # "band_05" :  inputPath + "R20m/" + granuleBandTemplate +  "B05_20m.jp2",
+    # "band_06" :  inputPath + "R20m/" + granuleBandTemplate +  "B06_20m.jp2",
+    # "band_07" :  inputPath + "R20m/" + granuleBandTemplate +  "B07_20m.jp2",
+    # "band_08" :  inputPath + "R10m/" + granuleBandTemplate +  "B08_10m.jp2",
+    # "band_8A" :  inputPath + "R20m/" + granuleBandTemplate +  "B8A_20m.jp2",
+    # "band_09" :  inputPath + "R60m/" + granuleBandTemplate +  "B09_60m.jp2",
+    # "band_WVP" :  inputPath + "R10m/" + granuleBandTemplate +  "WVP_10m.jp2",
+    # "band_11" :  inputPath + "R20m/" + granuleBandTemplate +  "B11_20m.jp2",
+    # "band_12" :  inputPath + "R20m/" + granuleBandTemplate +  "B12_20m.jp2"}
 
+    # 1C product
+    bands = {"band_01" :  inputPath + granuleBandTemplate +  "B01.jp2",
+    "band_02" :  inputPath + granuleBandTemplate +  "B02.jp2",
+    "band_03" :  inputPath + granuleBandTemplate +  "B03.jp2",
+    "band_04" :  inputPath + granuleBandTemplate +  "B04.jp2",
+    "band_05" :  inputPath + granuleBandTemplate +  "B05.jp2",
+    "band_06" :  inputPath + granuleBandTemplate +  "B06.jp2",
+    "band_07" :  inputPath + granuleBandTemplate +  "B07.jp2",
+    "band_08" :  inputPath + granuleBandTemplate +  "B08.jp2",
+    "band_8A" :  inputPath + granuleBandTemplate +  "B8A.jp2",
+    "band_09" :  inputPath + granuleBandTemplate +  "B09.jp2",
+    "band_10" :  inputPath + granuleBandTemplate +  "B10.jp2",
+    "band_11" :  inputPath + granuleBandTemplate +  "B11.jp2",
+    "band_12" :  inputPath + granuleBandTemplate +  "B12.jp2"}
 
-    cmd = ['gdalbuildvrt', '-resolution', 'user', '-tr' ,'20', '20', '-separate' ,outPutFullVrt]
-
+    # 10m resolution and bilinear resampling method
+    cmd = ['gdalbuildvrt', '-resolution', 'user', '-tr' ,'10', '10', '-separate', '-r', 'bilinear' ,outPutFullVrt]
 
     for band in sorted(bands.values()):
         cmd.append(band)
@@ -111,16 +125,11 @@ def generate_all_bands(unprocessedBandPath, granule, outputPathSubdirectory):
         # file exists
         subprocess.call(cmd)
 
-
-
     #params = ['', '-o', outPutFullPath, '-separate', band_01, band_02, band_03, band_04, band_05, band_06, band_07, band_08, band_8A, band_09, band_10, band_11, band_12]
 
     #gdal_merge.main(params)
 
     return(outPutFullPath)
-
-
-
 
 outputPath = '../Output/'
 readline.set_completer_delims(' \t\n;')
